b103e57596fc37e241952a6239a09b8d12b1cebb
anirbanivs <anirban@iviewsense.com>
2024-02-19 15:19:44 +0530

    file upload limited increased

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json({ limit: "20mb", extended: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true, parameterLimit: 50000 }));
 
 app.use("/api/users", usersRoutes);
 app.use("/api/heartbeat", heartbeatRoute);
