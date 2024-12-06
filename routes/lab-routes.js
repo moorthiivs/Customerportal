f51ffba2a9d833edc08100bbac6197c0cba18f4a
praveenivs <praveen@iviewsense.com>
2024-12-06 19:58:15 +0530

    lab info update added

--- routes/lab-routes.js
+++ routes/lab-routes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.post("/new", labController.addlab);
 
+router.post("/update", labController.updatelab);
+
 router.get("/fetchLab/:calibmaster_lab_id", labController.fetchLab);
 
 router.post("/postLabData", syncLabsController.postLabData)
