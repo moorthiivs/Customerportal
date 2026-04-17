01de9578a5520d794e6a4722e9c185341f09ffe0
moorthiivs <moorthi@iviewsense.com>
2026-04-17 15:50:06 +0530

    Configure dynamic environment variables for server URL and port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   ADMIN_PASSWORD: "Good2023$#1",
   TOKEN_SECRET: "CalibMaster_Secret",
-  CALIBMASTER_PORTAL_SERVER: "http://localhost:5000",
-  CALIBMASTER_PORT: 5000
+  CALIBMASTER_PORTAL_SERVER: process.env.CALIBMASTER_URL || "http://localhost:5000",
+  CALIBMASTER_PORT: process.env.CALIBMASTER_PORT || 5000
 };
